e8fb4b6b5643727ab4365204015eb819fdc8795e
chaitanya <chaitanyareddy781@gmail.com>
2026-05-20 20:15:48 -0400

    Renamed project to TouristManagementApp

--- src/main/java/com/example/TouristManagementAppApplication.java
+++ src/main/java/com/example/TouristManagementAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TouristBackendAppApplication {
+public class TouristManagementAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TouristBackendAppApplication.class, args);
+		SpringApplication.run(TouristManagementAppApplication.class, args);
 	}
 
 }
